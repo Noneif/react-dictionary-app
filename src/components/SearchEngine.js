6f868686d8077cd699263f50f2fa33aa3e2f1930
Noneif <sofar0222@gmail.com>
2022-08-19 18:30:13 +0300

    Added api

--- src/components/SearchEngine.js
+++ src/components/SearchEngine.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./SearchEngine.css";
 
 function SearchEngine() {
   const [keyword, setKeyword] = useState(null);
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function handleSubmit(event) {
     event.preventDefault();
-    alert(`Search for ${keyword} definition`);
+
+    let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    axios.get(apiUrl).then(handleResponse);
+    console.log(apiUrl);
   }
 
   function handleTyping(event) {
