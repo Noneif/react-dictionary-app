bc6bfbb7509b4edf24576b7dd19eb2065e47ecd4
Noneif <sofar0222@gmail.com>
2022-08-20 18:15:41 +0300

    +

--- src/components/Example.js
+++ src/components/Example.js
@@ -6,7 +6,7 @@ function Example(props) {
   if (props.example) {
     return (
       <div className="Example">
-        <strong>Example:</strong> {props.example}
+        <em>Example:</em> {props.example}
       </div>
     );
   } else {
